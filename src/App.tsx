9cf24cfc82fac345f151ffdb18d21b1f208059cf
Rohit Tokala <rtokala@purdue.edu>
2022-11-02 22:41:01 -0400

    Display random word on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test");
-  return <h1>Hello</h1>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  return <h1>{wordToGuess}</h1>;
 }
 
 export default App;
