4b8b59bce5de792d59013700c8ee2eeb24557050
Rohit Tokala <rtokala@purdue.edu>
2022-11-02 23:47:49 -0400

    Add visibility check to guessed letters

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,21 @@
 export function HangmanWord() {
-  return <div></div>;
+  const word = "test";
+  const guessedLetters = ["t", "e"];
+  return (
+    <div className=" flex gap-3 text-8xl font-mono font-medium uppercase">
+      {word.split("").map((letter, index) => (
+        <span className=" border-b-8 border-b-black" key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
