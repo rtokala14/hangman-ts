7fc68a222dd02492567e26715c8e5d90feb65309
Rohit Tokala <rtokala@purdue.edu>
2022-11-02 23:37:40 -0400

    Draw arms and legs

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,8 +1,34 @@
+const HEAD = (
+  <div className=" w-16 h-16 rounded-full border-black border-8 absolute top-14 -right-7" />
+);
+
+const BODY = <div className=" w-2 h-28 bg-black absolute top-28 right-0" />;
+
+const RIGHT_ARM = (
+  <div className=" w-24 h-2 bg-black absolute top-36 armRotateRight" />
+);
+
+const LEFT_ARM = (
+  <div className=" w-24 h-2 bg-black absolute top-36 armRotateLeft" />
+);
+
+const LEFT_LEG = <div className=" w-24 h-2 bg-black absolute legRotateLeft" />;
+
+const RIGHT_LEG = (
+  <div className=" w-24 h-2 bg-black absolute legRotateRight" />
+);
+
 export function HangmanDrawing() {
   return (
     <div className=" relative">
-      <div className=" h-14 w-3 bg-black absolute top-0 right-0" />
-      <div className=" h-3 w-52 bg-black ml-32" />
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {LEFT_LEG}
+      {RIGHT_LEG}
+      <div className=" h-16 w-3 bg-black absolute top-0 right-0" />
+      <div className=" h-3 w-56 bg-black ml-32" />
       <div className=" h-96 w-3 bg-black ml-32" />
       <div className=" h-3 w-64 bg-black" />
     </div>
